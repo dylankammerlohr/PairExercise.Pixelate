e41e9bb8a4e7a94bc6a2cdf7e4441ce191638a5b
Dylan Kammerlohr <dylankammerlohr@gmail.com>
2022-06-21 14:22:30 -0500

    add row

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // Your code here
+const table = document.getElementsByTagName('table')[0]
+
+function makeRow(){
+    const row = document.createElement('tr')
+    for (let i = 0; i < 20; i++){
+        const td = document.createElement('td')
+        row.appendChild(td)
+    }
+    table.appendChild(row)
+}
+
+const button = document.getElementById('add-row')
+button.addEventListener('click', makeRow)
